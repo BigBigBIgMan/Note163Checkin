8906b448a9fde5eaa1c2455c6811c5e5a11b1b01
BigBigBIgMan <1741999340@qq.com>
2023-04-29 10:28:11 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -100,6 +100,7 @@ async Task<(bool isInvalid, string result)> IsInvalid(string cookie)
     //每日打开客户端（即登陆）
     string result = await (await client.PostAsync("https://note.youdao.com/yws/api/daupromotion?method=sync", null))
         .Content.ReadAsStringAsync();
+         await Notify($"验证结果： {result }");
     return (result.Contains("error", StringComparison.OrdinalIgnoreCase), result);
 }
 
