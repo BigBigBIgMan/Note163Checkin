1f649083234bfeaab6264e487fe7bd29a124c8be
BigBigBIgMan <1741999340@qq.com>
2023-05-11 10:00:03 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -52,58 +52,60 @@ Console.WriteLine("redis:{0}", isRedis ? "有效" : "无效");
 //Console.WriteLine(resultStr);
 
 
-            //string serviceAddress = "https://www.rfidfans.com/upload/qiandao.php";
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serviceAddress);
+//string serviceAddress = "https://www.rfidfans.com/upload/qiandao.php";
+//HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serviceAddress);
  //
-            //request.Method = "POST";
-            //request.ContentType = "application/json";
-            //string strContent = "{ \"username\": \"xukuan\",\"passc\": \"MTIzNDU2\",\"USERID\": \"MTAwMDIxNjM2Mw==\"}";
-            ////string strContent = data; //参数data的格式就是上一句被注释的语句
-            //using (StreamWriter dataStream = new StreamWriter(request.GetRequestStream()))
-            //{
-            //    dataStream.Write(strContent);
-            //    dataStream.Close();
-            //}
-            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            //string encoding = response.ContentEncoding;
-            //if (encoding == null || encoding.Length < 1)
-            //{
-            //    encoding = "UTF-8"; //默认编码  
-            //}
-            //StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
-            //string retString = reader.ReadToEnd();
-            //Console.WriteLine(retString);
-//Encoding encoding = Encoding.GetEncoding("UTF-8");
-Stream outstream = null;
+ //request.Method = "POST";
+ //request.ContentType = "application/json";
+ //string strContent = "{ \"username\": \"xukuan\",\"passc\": \"MTIzNDU2\",\"USERID\": \"MTAwMDIxNjM2Mw==\"}";
+ ////string strContent = data; //参数data的格式就是上一句被注释的语句
+ //using (StreamWriter dataStream = new StreamWriter(request.GetRequestStream()))
+ //{
+ //    dataStream.Write(strContent);
+ //    dataStream.Close();
+ //}
+ //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+ //string encoding = response.ContentEncoding;
+ //if (encoding == null || encoding.Length < 1)
+ //{
+ //    encoding = "UTF-8"; //默认编码  
+ //}
+ //StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+ //string retString = reader.ReadToEnd();
+ //Console.WriteLine(retString);
+try
+{
+            Encoding encoding = Encoding.UTF8;
+            Stream outstream = null;
             Stream instream = null;
             StreamReader sr = null;
-                  string url = "https://www.rfidfans.com/upload/qiandao.php";
+            string url = "https://www.rfidfans.com/upload/qiandao.php";
             HttpWebRequest request = null;
             HttpWebResponse response = null;
-
-// 准备请求,设置参数
+            
+            // 准备请求,设置参数
             request = WebRequest.Create(url) as HttpWebRequest;
             request.Method = "POST";
-            request.ContentType ="application/x-www-form-urlencoded";
-           
-            byte[] data = Encoding.UTF8.GetBytes("username=xukuan&passc=MTIzNDU2&USERID=MTAwMDIxNjM2Mw==");
+            request.ContentType ="application/x-www-form-urlencoded";           
+            byte[] data = encoding.GetBytes("username=xukuan&passc=MTIzNDU2&USERID=MTAwMDIxNjM2Mw==");
             request.ContentLength = data.Length;
             outstream = request.GetRequestStream();
             outstream.Write(data, 0, data.Length);
             outstream.Flush();
             outstream.Close();
             //发送请求并获取相应回应数据
-
-
             response = request.GetResponse() as HttpWebResponse;
             //直到request.GetResponse()程序才开始向目标网页发送Post请求
             instream = response.GetResponseStream();
-            sr = new StreamReader(instream, Encoding.UTF8);
+            sr = new StreamReader(instream, encoding);
             //返回结果网页(html)代码
-
-
             string content = sr.ReadToEnd();
             Console.WriteLine(content);
+}
+catch(Exception)
+{
+            Console.WriteLine("你输入的值为空");
+}
 
    #endregion
 Console.WriteLine("有道云笔记签到开始运行...");
