ea7f92a1b5751c9acdc81b1c207a9a9eca652369
BigBigBIgMan <1741999340@qq.com>
2023-08-01 16:13:09 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -285,12 +285,8 @@ async Task Notify(string msg, string text)
     await pushPlusNotify(msg,text);
     Console.WriteLine(msg);
     text = text+"\n\n \n\n "+msg;
-    await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
-}
-
-async Task pushPlusNotify(string msg, string text)
-{
     await _scClient.GetAsync($"http://www.pushplus.plus/send?token={_conf.PpToken}&title={msg}&content={text}");
+    await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
 }
 
 T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
