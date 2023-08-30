a50fbfa6cae68a30dfb38ebd9a749a175c03144a
BigBigBIgMan <1741999340@qq.com>
2023-08-30 15:38:00 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -288,10 +288,10 @@ async Task Notify(string msg, string text)
    Console.WriteLine(pushplus);
     var ftqq = await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
    Console.WriteLine(ftqq);
-    var ftqq = await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
-   Console.WriteLine(ftqq);
-    var ftqq = await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
-   Console.WriteLine(ftqq);
+    var ftqq1 = await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
+   Console.WriteLine(ftqq1);
+    var ftqq2 = await _scClient.GetAsync($"https://sc.ftqq.com/{_conf.ScKey}.send?title={msg}&desp={text}");
+   Console.WriteLine(ftqq2);
 }
 
 T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
